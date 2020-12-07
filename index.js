c15a86f8bd8d091670ac36ab0a3ad9142c11bff7
danmarrod <danmarrod@alum.us.es>
2020-12-07 19:48:12 +0100

    Installed Swagger

--- index.js
+++ index.js
@@ -2,6 +2,34 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var dataStore = require('nedb');
 
+const expressSwagger = require('express-swagger-generator');
+let options = {
+    swaggerDefinition: {
+        info: {
+            description: 'This is a sample server',
+            title: 'Swagger',
+            version: '1.0.0',
+        },
+        host: 'localhost:3000',
+        basePath: '/v1',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http', 'https'],
+		securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: ['./routes/**/*.js'] //Path to the API handle folder
+};
+
 var port = (process.env.PORT || 3000);
 var BASE_API_PATH = "/api/v1";
 var DB_FILE_NAME = __dirname + "/contacts.json";
@@ -49,5 +77,6 @@ app.post(BASE_API_PATH + "/contacts", (req, res) => {
     
 });
 
+expressSwagger(options).
 app.listen(port);
 console.log("Server ready!");

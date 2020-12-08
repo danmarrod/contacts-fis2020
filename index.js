4856f7bea3d4ede88dd63b52ab12287814aed2a1
danmarrod <danmarrod@alum.us.es>
2020-12-08 03:31:04 +0100

    Fixed error wit heroky deployment

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let options = {
             title: 'Swagger',
             version: '1.0.0',
         },
-        host: 'fis-contacts.herokuapp.com',
+        host: 'localhost:3000',
         basePath: BASE_API_PATH,
         produces: [
             "application/json"
